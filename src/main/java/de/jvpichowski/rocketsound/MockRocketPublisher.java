740dbf24f365fe18c56f228ad6709a6ee99fdaf1
Jan von Pichowski <janvonpichowski@gmail.com>
2021-02-28 19:20:51 +0100

    Removed messages from mock

--- src/main/java/de/jvpichowski/rocketsound/MockRocketPublisher.java
+++ src/main/java/de/jvpichowski/rocketsound/MockRocketPublisher.java
@@ -31,19 +31,19 @@ public final class MockRocketPublisher extends AbstractVerticle {
 		config = Config.get(forcedConfig, config(), Configuration.class);
 
 		vertx.setPeriodic(Duration.ofSeconds(1).toMillis(), h -> {
-			vertx.eventBus().publish(config.address, new Amplitude(3.7).json());
-			vertx.eventBus().publish(config.address,
-					new Spectrum(2.7, 1004.3, new double[]{2.6, 0.0, 3.5, 100, 980.5}).json());
-			vertx.eventBus().publish(config.address,
-					new GpsData(3, 7, 4343345.0, -376.322, 42134894).json());
+			//vertx.eventBus().publish(config.address, new Amplitude(3.7).json());
+			//vertx.eventBus().publish(config.address,
+			//		new Spectrum(2.7, 1004.3, new double[]{2.6, 0.0, 3.5, 100, 980.5}).json());
+			//vertx.eventBus().publish(config.address,
+			//		new GpsData(3, 7, 4343345.0, -376.322, 42134894).json());
 			vertx.eventBus().publish(config.address, new DbResponse(NineDofData.class, Arrays.asList(new NineDofData(
 					new Accelerometer(0.47, 3.5, 1.0),
 					new Gyroscope(0.3, -7.2, -0.1),
 					new Magnetometer(0.4, 28, -0.33)).json())).json());
-			vertx.eventBus().publish(config.address, new BaroData(
-					new Pressure(67773.3),
-					new Temperature(24.3),
-					new Altitude(287.0)).json());
+			//vertx.eventBus().publish(config.address, new BaroData(
+			//		new Pressure(67773.3),
+			//		new Temperature(24.3),
+			//		new Altitude(287.0)).json());
 		});
 		startPromise.complete();
 	}
