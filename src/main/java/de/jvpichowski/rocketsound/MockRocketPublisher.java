5ad57a93e1cf22bb6fd0ddabe9661e013421bc00
Jan von Pichowski <janvonpichowski@gmail.com>
2021-02-28 19:08:42 +0100

    Added basic publishing

--- src/main/java/de/jvpichowski/rocketsound/MockRocketPublisher.java
+++ src/main/java/de/jvpichowski/rocketsound/MockRocketPublisher.java
@@ -7,8 +7,11 @@ import de.jvpichowski.rocketsound.messages.sound.Spectrum;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import org.telestion.api.config.Config;
+import org.telestion.core.database.DbResponse;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 
 public final class MockRocketPublisher extends AbstractVerticle {
 
@@ -33,10 +36,10 @@ public final class MockRocketPublisher extends AbstractVerticle {
 					new Spectrum(2.7, 1004.3, new double[]{2.6, 0.0, 3.5, 100, 980.5}).json());
 			vertx.eventBus().publish(config.address,
 					new GpsData(3, 7, 4343345.0, -376.322, 42134894).json());
-			vertx.eventBus().publish(config.address, new NineDofData(
+			vertx.eventBus().publish(config.address, new DbResponse(NineDofData.class, Arrays.asList(new NineDofData(
 					new Accelerometer(0.47, 3.5, 1.0),
 					new Gyroscope(0.3, -7.2, -0.1),
-					new Magnetometer(0.4, 28, -0.33)).json());
+					new Magnetometer(0.4, 28, -0.33)).json())).json());
 			vertx.eventBus().publish(config.address, new BaroData(
 					new Pressure(67773.3),
 					new Temperature(24.3),
